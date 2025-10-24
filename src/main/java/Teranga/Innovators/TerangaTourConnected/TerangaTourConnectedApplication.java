a9a76b382b852ac83e1ad917cc9059cdb0872400
MKB713 <bakhadija742@gmail.com>
2025-10-24 14:53:31 +0000

    1er commit

---
+++ src/main/java/Teranga/Innovators/TerangaTourConnected/TerangaTourConnectedApplication.java
@@ -0,0 +1,13 @@
+package Teranga.Innovators.TerangaTourConnected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerangaTourConnectedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerangaTourConnectedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Teranga/Innovators/TerangaTourConnected/TerangaTourConnectedApplicationTests.java
@@ -0,0 +1,13 @@
+package Teranga.Innovators.TerangaTourConnected;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerangaTourConnectedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
